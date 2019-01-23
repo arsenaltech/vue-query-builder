81bbbd9073a9661c971b3103d7f8096e7529c81c
Mehul <mehul.ce@gmail.com>
2019-01-22 18:58:51 -0800

    CSS update

--- src/components/QueryBuilderGroup.vue
+++ src/components/QueryBuilderGroup.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="vqb-group font-secondary">
     <div class="vqb-group-heading">
-      <div class="match-type-container mb-2">
+      <div class="flex bg-50 p-3">
         <div>
           <label for="vqb-match-type" class="pr-2">{{ labels.matchType }}</label>
           <select id="vqb-match-type" class="bg-grey-lighter rounded transition h-6 px-4 mx-2" v-model="query.logicalOperator">
